b739dfc5b0c1eed367223a066c96e87360122a1d
Anjolaanuoluwapo <anjolaakinsoyinu@gmail.com>
2025-04-18 14:26:26 +0100

    fix vlucas phpdotent pointing to public/ instead of /

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 require '../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__,".env");
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/../" ,".env");
 $dotenv->load();
 
 require '../core/Router.php';
